5c3c7b4861d160df2674cd1d1ada3e14a24a9fd3
Adam Aldous <aldousadam@gmail.com>
2016-04-11 16:35:22 -0600

    created challenge controller

---
+++ ChallengeController.swift
@@ -0,0 +1,30 @@
+//
+//  ChallengeController.swift
+//  BopMe
+//
+//  Created by Adam Aldous on 4/11/16.
+//  Copyright © 2016 MichaelMecham. All rights reserved.
+//
+
+import Foundation
+
+class ChallengeController {
+    
+    static let sharedController = ChallengeController()
+    
+    var challenge: Challenge?
+    
+    var timerInterval: Int = 5
+    
+    var timer: NSTimer?
+    
+    
+    /*
+     func d
+     
+     func pickRandomChallenge() -> Challenge
+     
+     func updateWithNextChallenge(Challenge)
+     
+    */
+}
